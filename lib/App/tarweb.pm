0e934fd3a8977d6c32dda88cbbcf21a6a5fc82d7
Graham Ollis <plicease@cpan.org>
2022-09-01 11:49:34 -0600

    basic structure

--- lib/App/tarweb.pm
+++ lib/App/tarweb.pm
@@ -2,11 +2,32 @@ package App::tarweb;
 
 use strict;
 use warnings;
-use 5.008004;
+use 5.020;
+use experimental qw( signatures postderef );
 
 # ABSTRACT: Open an archive file in your web browser!
 # VERSION
 
+=head1 SYNOPSIS
+
+ $ tarweb foo.tar.gz
+
+=head1 DESCRIPTION
+
+This class just contains the machinery of the L<tarweb> application.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<tarweb>
+
+=back
+
+=cut
+
+sub main ($, @ARGV)
+{
+}
+
 1;
-
-
